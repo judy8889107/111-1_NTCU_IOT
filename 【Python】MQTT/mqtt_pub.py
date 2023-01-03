e86cc2749f092ff17bcdcc7ef901f1b4d9550087
judy <judy8889107@gmail.com>
2023-01-03 17:47:53 +0800

    [Python]連上ThingSpeak的MQTT做pub,sub

--- 【Python】MQTT/mqtt_pub.py
+++ 【Python】MQTT/mqtt_pub.py
@@ -1,9 +1,18 @@
 import paho.mqtt.client as mqtt
 
-MQTTServerIP = "192.168.5.191"
-MQTTServerPort = 1883 #port
-MQTTTopicName = "IoT" #TOPIC name
+# https://www.cooooder.com/archives/20210303
 
-mqttc = mqtt.Client("python_pub")  # mqtt.Client("ClientID")
-mqttc.connect(MQTTServerIP, MQTTServerPort)
-mqttc.publish(MQTTTopicName, "Hello")
+mqtt_host = "mqtt3.thingspeak.com"
+channel_ID = "1760300"
+write_api_key = "F9RRUQHAY5K3NNY8"
+mqtt_client_id = "ER4HCSgfIhAUDA0GMiEmBgU"
+mqtt_username = "ER4HCSgfIhAUDA0GMiEmBgU"
+mqtt_password = "Op8uqXS4J6ivQlo+l9qlFslC"
+mqtt_port = 1883 #port
+mqtt_topic = "channels/" + channel_ID + "/publish/fields/field1" #TOPIC name
+payload = "100"
+
+mqttc = mqtt.Client(mqtt_client_id)  # 构造方法 Client(client_id="", clean_session=True, userdata=None, protocol=MQTTv311, transport="tcp")
+mqttc.username_pw_set(mqtt_username, mqtt_password)
+mqttc.connect(mqtt_host, mqtt_port) # connect(host, port=1883, keepalive=60, bind_address="")
+mqttc.publish(mqtt_topic, payload)
--- 【Python】MQTT/mqtt_sub.py
+++ 【Python】MQTT/mqtt_sub.py
@@ -1,11 +1,26 @@
 import paho.mqtt.client as mqtt
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    client.subscribe("IoT")
+    channel_ID = "1760300"
+    mqtt_topic = "channels/" + channel_ID + "/publish/fields/field1" # 需同樣TOPIC
+    # 不能像 MQTT.fx一樣用 channels/channelID/subscribe
+    client.subscribe(mqtt_topic)
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
-client = mqtt.Client()
+
+mqtt_host = "mqtt3.thingspeak.com"
+
+write_api_key = "F9RRUQHAY5K3NNY8"
+mqtt_client_id = "ER4HCSgfIhAUDA0GMiEmBgU"
+mqtt_username = "ER4HCSgfIhAUDA0GMiEmBgU"
+mqtt_password = "Op8uqXS4J6ivQlo+l9qlFslC"
+mqtt_port = 1883 #port
+
+
+
+client = mqtt.Client(mqtt_client_id)
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect("192.168.5.191", 1883, 60) # connect(host, port=1883, keepalive=60, bind_address="")
+client.username_pw_set(mqtt_username, mqtt_password)
+client.connect(mqtt_host, mqtt_port, 60) # connect(host, port=1883, keepalive=60, bind_address="")
 client.loop_forever()
